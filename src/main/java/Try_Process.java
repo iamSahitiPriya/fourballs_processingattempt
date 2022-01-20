727b18c2fd20aa08e305653da96852886c87ce55
iamSahitiPriya <smssaddepalli@gmail.com>
2022-01-20 17:12:25 +0530

    commit in fourballs

--- src/main/java/Try_Process.java
+++ src/main/java/Try_Process.java
@@ -1,50 +1,35 @@
 import processing.core.PApplet;
 public class Try_Process extends PApplet {
-       int speed1=0;int speed2=0;int speed3=0; int speed4=0;
-       int height=500;
-      public static void main(String args[]) {
+    Ellipse e1;
+    Ellipse e2;
+    Ellipse e3;
+    Ellipse e4;
 
-              PApplet.main("Try_Process");
-       }
-       @Override
-       public void settings() {
-              super.settings();
-              size(640, 480);
-       }
-       @Override
-       public void setup()
-       {
-              //ellipse
-       }
-       public void draw() {
-             firstellipse();
-              secondellipse();
-              thirdellipse();
-              fourthellipse();
-       }
-       public void firstellipse()
-       {
-              ellipse(speed1,height/5,20,20);
-              speed1++;
-       }
-       public void secondellipse()
-       {
-              ellipse(speed2,2*height/5,20,20);
-              speed2=speed2+2;
-       }
-       public void thirdellipse()
-       {
-              ellipse(speed3,3*height/5,20,20);
-              speed3=speed3+3;
-       }
-       public void fourthellipse()
-       {
-              ellipse(speed4,4*height/5,20,20);
-              speed4=speed4+4;
-       }
+    public static void main(String args[]) {
+
+        PApplet.main("Try_Process");
+    }
+    @Override
+    public void settings() {
+        super.settings();
+        size(640,500);
+    }
+    @Override
+    public void setup()
+    {
+
+        e1=new Ellipse(this,1);
+        e2=new Ellipse(this,2);
+        e3=new Ellipse(this,3);
+        e4=new Ellipse(this,4);
+
+        }
+    public void draw() {
+        e1.firstellipse();
+        e2.secondellipse();
+        e3.thirdellipse();
+        e4.fourthellipse();
+
+    }
 
 }
-
-
-
-
